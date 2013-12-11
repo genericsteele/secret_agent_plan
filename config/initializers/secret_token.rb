e9c12f7cd2867790b92113e3424fcca801b2b9a1
Eric Steele <eric@iamericsteele.com>
2013-12-10 20:38:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecretAgentPlan::Application.config.secret_key_base = 'd9b35a7f3ae3cba0ea216bc498f0d8858c9da7a724c8f45171b7101c82113938210c92390676e7b54748c8c45bb91d54cc6b9f26772e0e4dce0ff835a3346deb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SecretAgentPlan::Application.config.session_store :cookie_store, key: '_secret_agent_plan_session'
